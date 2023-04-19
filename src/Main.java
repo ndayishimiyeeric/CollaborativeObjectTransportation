8d7f2ce7a661d4ffde294bc5aa7220a5990c8fdf
ndayishimiyeeric <ndayishimiyeeric86@gmail.com>
2023-04-20 00:54:08 +0200

    add and test the javaFX library

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,30 @@
-import jade.core.Profile;
-import jade.core.ProfileImpl;
-import jade.wrapper.AgentContainer;
-import jade.wrapper.AgentController;
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        StackPane layout = new StackPane();
+
+        Button button = new Button("Get java version");
+
+        button.setOnAction(actionEvent -> {
+            System.out.println(System.getProperty("java.version"));
+        });
+
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 300);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("MultiAgent System");
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {launch();}
 }
